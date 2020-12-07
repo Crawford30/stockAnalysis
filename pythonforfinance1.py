ba6f05edb69bbf6a877bc2fcbbffee6e1612fb71
joel <joelcrawford30@gmail.com>
2020-12-07 16:33:49 +0300

    data frame

--- pythonforfinance1.py
+++ pythonforfinance1.py
@@ -6,16 +6,23 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12, 31)
+df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
 
-ticker_dict = {}
-for idx, ticker in enumerate(['TSLA']):
-    try:
-        df_ticker = web.DataReader(ticker, 'yahoo', start, end)
-        ticker_dict[ticker] = df_ticker['Close']
-    except:
-        pass
-stocks = pd.DataFrame(ticker_dict)
+#print(df.head())
 
-print(stocks.tail())
+df.plot()
+plt.show()
+
+# start = dt.datetime(2000, 1, 1)
+# end = dt.datetime(2016, 12, 31)
+#
+# ticker_dict = {}
+# for idx, ticker in enumerate(['TSLA']):
+#     try:
+#         df_ticker = web.DataReader(ticker, 'yahoo', start, end)
+#         ticker_dict[ticker] = df_ticker['Close']
+#     except:
+#         pass
+# stocks = pd.DataFrame(ticker_dict)
+#
+# stocks.to_csv('tsla.csv')
