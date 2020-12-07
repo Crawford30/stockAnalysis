b96e5b2afdc0e88ac58380045a1b49fb8623bd81
joel <joelcrawford30@gmail.com>
2020-12-07 17:29:02 +0300

    moving average plot and subplots

--- pythinforfinance1.py
+++ pythinforfinance1.py
@@ -7,17 +7,27 @@ import pandas_datareader.data as web
 style.use('ggplot')
 
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+#df.dropna(inplace=True)
+print(df.head())
 
-#print(df.head())
+#subplots are called axis
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
 
-df.plot()
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
+
+
 
 # start = dt.datetime(2000, 1, 1)
 # end = dt.datetime(2016, 12, 31)
-#
+
 # ticker_dict = {}
-# for idx, ticker in enumerate(['TSLA']):
+# for idx, ticker in enumerate(['TSLA', 'IBM', 'LNKD']):
 #     try:
 #         df_ticker = web.DataReader(ticker, 'yahoo', start, end)
 #         ticker_dict[ticker] = df_ticker['Close']
@@ -25,4 +35,4 @@ plt.show()
 #         pass
 # stocks = pd.DataFrame(ticker_dict)
 #
-# stocks.to_csv('tsla.csv')
+# print(stocks.tail())
